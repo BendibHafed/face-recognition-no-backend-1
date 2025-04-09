e80a8034fed1441ca2d6382ba1a2059bddf8ac68
BendibHafed <bendibhafededdine@gmail.com>
2025-04-09 03:52:29 +0200

    updating package.json && vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/face-recognition-no-backend-1",
   server: {
     proxy: {
       '/clarifai': {  // 👈 Requests starting with `/clarifai` will be proxied
